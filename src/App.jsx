b27b03e95e355e68b45fb195d94a023575e86480
Saad <saadniazifed@gmail.com>
2023-07-30 13:20:16 +0500

    Personal Details section of the left side. Adding error validation using ReactJS, HTML, CSS and not any third party library.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,69 @@
 import FormInput from "./components/FormInput";
+import React, { useState } from "react";
 
 function App() {
+  const [personalDetails, setPersonalDetails] = useState({
+    fullName: "",
+    email: "",
+    phoneNumber: "",
+    address: "",
+  });
+
+  const personalDetailsInput = [
+    {
+      label: "Full Name",
+      name: "fullName",
+      placeholder: "Enter your full name",
+      type: "text",
+      id: 1,
+      errorMessage:
+        "Full Name Should Not Contain Any Numbers Or Special Characters. Max Limit is 25 words.",
+    },
+    {
+      label: "Email",
+      name: "email",
+      placeholder: "Enter your email",
+      type: "email",
+      id: 2,
+      errorMessage: "It Should Be A Valid Email Address. Please Try Again.",
+    },
+    {
+      label: "Phone Number",
+      name: "phoneNumber",
+      placeholder: "Enter your phone number",
+      type: "tel",
+      id: 3,
+    },
+    {
+      label: "Address",
+      name: "address",
+      placeholder: "Enter your address",
+      type: "text",
+      id: 4,
+    },
+  ];
+
+  const onChange = (e) => {
+    setPersonalDetails({ ...personalDetails, [e.target.name]: e.target.value });
+  };
+
+  console.log(personalDetails);
+
   return (
     <div className="flex justify-evenly min-h-screen">
       <div className="w-2/5 border p-4 bg-gray-100 min-h-screen">
         <h3 className="font-sans">Personal Details</h3>
         <form className="mt-3">
-          <FormInput placeholder={"Full name"} />
-          <FormInput placeholder={"Email"} />
-          <FormInput placeholder={"Phone Number"} />
-          <FormInput placeholder={"Address"} />
+          {personalDetailsInput.map((input) => (
+            <React.Fragment key={input.id}>
+              <FormInput
+                {...input}
+                value={personalDetails[personalDetailsInput.name]}
+                onChange={onChange}
+                errorMessage={input.errorMessage}
+              />
+            </React.Fragment>
+          ))}
         </form>
       </div>
       <div className="w-2/5 border p-4 bg-gray-200 min-h-screen">
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,20 +1,25 @@
 import PropTypes from "prop-types";
 
 const FormInput = (props) => {
-  const { placeholder } = props;
+  const { label, onChange, errorMessage, ...input } = props;
   return (
-    <div className="flex justify-between mb-2">
-      {/* <label className="text-left mr-3">Username</label> */}
+    <div className="mb-3">
+      <label className="block font-medium mb-1" style={{ minWidth: "120px" }}>
+        {label}
+      </label>
       <input
-        className="flex-grow font-sans px-4 py-2 border rounded-md border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 text-gray-600"
-        placeholder={placeholder}
+        className="w-full px-4 py-2 border rounded-md border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 text-gray-600"
+        {...input}
+        onChange={onChange}
       />
+      <span className="text-red-500 text-sm">{errorMessage}</span>
     </div>
   );
 };
 
 FormInput.propTypes = {
-  placeholder: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
 
 export default FormInput;
