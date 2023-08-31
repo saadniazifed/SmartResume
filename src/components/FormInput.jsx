3e033aff5085bbe5ef6d4841c0d83a6444dde19a
Saad Niazi <saadniazifed@gmail.com>
2023-08-31 22:23:57 +0500

    Worked on making the Resume look nicerusing tailwindcss.

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -15,19 +15,21 @@ const FormInput = (props) => {
       </label>
       {isTextArea ? (
         <textarea
-          className="w-full bg-[#292929] text-white px-4 py-5 border rounded-md border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 text-gray-600"
+          className="w-full bg-[#292929] text-white px-4 py-5 border rounded-md border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
           {...input}
           onChange={onChange}
           value={value}
           maxLength={150}
         />
       ) : (
-        <input
-          className="w-full bg-[#292929]  px-4 py-2 border rounded-md border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 text-white"
-          {...input}
-          onChange={onChange}
-          value={value}
-        />
+        <>
+          <input
+            className="w-full bg-[#292929]  px-4 py-2 border rounded-md border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 text-white"
+            {...input}
+            onChange={onChange}
+            value={value}
+          />
+        </>
       )}
     </div>
   );
--- src/components/PersonalDetails/RenderPersonalDetailsInput.jsx
+++ src/components/PersonalDetails/RenderPersonalDetailsInput.jsx
@@ -1,4 +1,12 @@
 import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faPhone,
+  faMapMarkerAlt,
+  faAlignLeft,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
 const RenderPersonalDetailsInput = (props) => {
   const { personalDetails, avatarImage } = props;
@@ -7,29 +15,39 @@ const RenderPersonalDetailsInput = (props) => {
     <div className="h-1/4 flex flex-col">
       {/* Centered text in the top div */}
       <div className="flex justify-between items-center h-1/4">
-        <p className="text-4xl font-bold text-white font-alfa">
-          {personalDetails.fullName}
-        </p>
-        <img
-          className="rounded-full w-16 h-16 object-cover"
-          src={avatarImage}
-          alt="Avatar"
-        />
+        <div className="flex items-center justify-between border-sky-400 border flex-[0.65]">
+          <FontAwesomeIcon icon={faUser} color="white" />
+          <p className="text-4xl font-bold text-white font-alfa">
+            {personalDetails.fullName}
+          </p>
+        </div>
+
+        <div>
+          <img
+            className="rounded-full w-16 h-16 object-cover self-end"
+            src={avatarImage}
+            alt="Avatar"
+          />
+        </div>
       </div>
 
       {/* Bottom div with labels and rendering vertically aligned */}
       <div className="flex flex-col h-3/4 w-full">
         <div className="text-left text-xl font-sans w-full md:w-auto">
-          <p>
-            <span className="font-bold font-sans"></span>{" "}
+          <p className="flex">
+            <span className="font-bold font-sans">
+              <FontAwesomeIcon icon={faEnvelope} color="white" />
+            </span>
             <span className="text-white rounded p-2 flex w-full justify-between h-8 items-center">
               <span>{personalDetails.email}</span>
             </span>
           </p>
         </div>
         <div className="text-left text-xl w-full md:w-auto">
-          <p>
-            <span className="font-bold font-sans"></span>{" "}
+          <p className="flex">
+            <span className="font-bold font-sans">
+              <FontAwesomeIcon icon={faPhone} color="white" />
+            </span>
             <span className="text-white rounded p-2 flex w-full justify-between h-8 items-center">
               <span>{personalDetails.phoneNumber}</span>
             </span>
@@ -37,7 +55,9 @@ const RenderPersonalDetailsInput = (props) => {
         </div>
         <div className="text-left text-xl w-full md:w-auto">
           <p>
-            <span className="font-bold font-sans"></span>{" "}
+            <span className="font-bold font-sans">
+              <FontAwesomeIcon icon={faMapMarkerAlt} color="white" />
+            </span>
             <span className="text-white rounded p-2 flex w-full justify-between h-8 items-center">
               <span>{personalDetails.address}</span>
             </span>
@@ -46,7 +66,9 @@ const RenderPersonalDetailsInput = (props) => {
 
         <div className="text-left text-xl w-full md:w-auto">
           <p>
-            <span className="font-bold text-white font-sans"></span>{" "}
+            <span className="font-bold text-white font-sans">
+              <FontAwesomeIcon icon={faAlignLeft} color="white" />
+            </span>
             <textarea className="text-white rounded p-2 w-full inline-block break-all bg-transparent">
               {personalDetails.smallDescription}
             </textarea>
